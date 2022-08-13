06f64924ee41e2ba6060fb15b7199235acd9e7a6
KritagyaChopra <85625671+KritagyaChopra@users.noreply.github.com>
2022-08-13 21:30:58 +0530

    Update Loglevels.java

--- Loglevels.java
+++ Loglevels.java
@@ -1,54 +1,47 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        if(logLine.substring(0,7).compareTo("[ERROR]") == 0){
-            String output = logLine.substring(logLine.indexOf(" ")+1,logLine.length());
-            return output;
+        if(logLine.charAt(1) == 'E'){
+            String s = logLine.substring(9,logLine.length()).trim();
+            return s;
         }
-        else if(logLine.substring(0,6).compareTo("[INFO]") == 0){
-            String output = logLine.substring(logLine.indexOf(" ")+1,logLine.length());
-            return output;
+        else if(logLine.charAt(1) == 'W'){
+            String s = logLine.substring(11,logLine.length()).trim();
+            return s;
         }
-        else if(logLine.substring(0,9).compareTo("[WARNING]") == 0){
-            String output = logLine.substring(logLine.indexOf(" ")+1,logLine.length()).trim();
-            return output;
+        else{
+            String s = logLine.substring(8,logLine.length()).trim();
+            return s;
         }
-        return "";
-    }
+        
+        }
 
     public static String logLevel(String logLine) {
-        if(logLine.substring(0,7).compareTo("[ERROR]") == 0){
-            String input = logLine.substring(1,6).toLowerCase() ;
-            return input;
+        if(logLine.charAt(1) == 'E'){
+            return "error";
         }
-        else if(logLine.substring(0,6).compareTo("[INFO]") == 0){
-            String input = logLine.substring(1,5).toLowerCase() ;
-            return input;
+        else if(logLine.charAt(1) == 'W'){
+            return "warning";
         }
-        else if(logLine.substring(0,9).compareTo("[WARNING]") == 0){
-            String input = logLine.substring(1,8).toLowerCase() ;
-            return input;
+        else{
+            return "info";
         }
-        return "";
+        
     }
 
     public static String reformat(String logLine) {
-         String input = "";
-        if(logLine.substring(0,7).compareTo("[ERROR]") == 0){
-            input = input  + " " + "(" + logLine.substring(1,6).toLowerCase() + ")" ;
-            String output = logLine.substring(logLine.indexOf(" ")+1,logLine.length()).trim() + input;
-            return output;
+        if(logLine.charAt(1) == 'E'){
+            String s = logLine.substring(9,logLine.length()).trim()+ " (error)";
+            return s;
         }
-        else if(logLine.substring(0,6).compareTo("[INFO]") == 0){
-            input = input + " " + "(" + logLine.substring(1,5).toLowerCase() + ")" ;
-            String output = logLine.substring(logLine.indexOf(" ")+1,logLine.length()).trim() + input;
-            return output;
+        else if(logLine.charAt(1) == 'W'){
+            String s = logLine.substring(11,logLine.length()).trim()+" (warning)";
+            return s;
         }
-        else if(logLine.substring(0,9).compareTo("[WARNING]") == 0){
-            input = input  + " " + "(" + logLine.substring(1,8).toLowerCase() + ")" ;
-            String output = logLine.substring(logLine.indexOf(" ")+1,logLine.length()).trim() + input ;
-            return output;
+        else{
+            String s = logLine.substring(8,logLine.length()).trim()+ " (info)";
+            return s;
         }
-        return "";
+        
     }
 }
