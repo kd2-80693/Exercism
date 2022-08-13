dc7e0d940f6b843397c953ae3d121ef51f53f3c1
KritagyaChopra <85625671+KritagyaChopra@users.noreply.github.com>
2022-08-13 21:29:25 +0530

    Update Lasagna.java

--- Lasagna.java
+++ Lasagna.java
@@ -3,25 +3,19 @@ public class Lasagna {
     public int expectedMinutesInOven(){
         return 40;
     }
+
     // TODO: define the 'remainingMinutesInOven()' method
-    public int remainingMinutesInOven(int timeAlreadyTaken){
-        int n =expectedMinutesInOven();
-        return n-timeAlreadyTaken;
+    public int remainingMinutesInOven(int a){
+        return(40-a);
     }
+    
     // TODO: define the 'preparationTimeInMinutes()' method
-    public int preparationTimeInMinutes(int layers){
-        return layers*2;
-    }
+    public int preparationTimeInMinutes(int a){
+        return(2*a);
+    }   
+
     // TODO: define the 'totalTimeInMinutes()' method
-    public static int totalTimeInMinutes(int layers, int timeAlreadyTaken){
-        int timeInPrepL = layers*2;
-        return timeAlreadyTaken + timeInPrepL;
-    }
-    public static void main(String args[]){
-        Lasagna lasagna = new Lasagna();
-        lasagna.expectedMinutesInOven();
-         lasagna.remainingMinutesInOven(30);
-         lasagna.preparationTimeInMinutes(2);
-         totalTimeInMinutes(8, 8);
+    public int totalTimeInMinutes(int a , int b){
+        return(2*a)+(b);
     }
 }
