46475c6135130e1b8cd44a9a60e56dc4b38aec93
KritagyaChopra <85625671+KritagyaChopra@users.noreply.github.com>
2022-08-14 21:30:25 +0530

    Create Darts.java

---
+++ Darts.java
@@ -0,0 +1,25 @@
+class Darts {
+    double x ;
+    double y ;
+
+    Darts(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int score() {
+        if(this.x*this.x + this.y * this.y >100){
+            return 0;
+        }
+        else if(this.x*this.x + this.y * this.y >25){
+            return 1;
+        }
+        else if(this.x*this.x + this.y * this.y >1){
+            return 5;
+        }
+        else{
+            return 10;
+        }
+    }
+
+}
