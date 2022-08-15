1c394634643de223d8dc4561520f44af244ee540
KritagyaChopra <85625671+KritagyaChopra@users.noreply.github.com>
2022-08-15 15:24:47 +0530

    Create BirdWatcher.java

---
+++ BirdWatcher.java
@@ -0,0 +1,51 @@
+import java.lang.* ;
+class BirdWatcher {
+    private final int[] birdsPerDay;
+     
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        return birdsPerDay;
+    }
+
+    public int getToday() {
+        if(birdsPerDay.length>0)
+            return birdsPerDay[birdsPerDay.length -1] ; 
+        return 0;
+    }
+
+    public void incrementTodaysCount() {
+        birdsPerDay[birdsPerDay.length - 1]++;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        for(int i : birdsPerDay){
+            if(i==0){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        int sum = 0 ;
+        for(int i = 0 ; i < Math.min(numberOfDays ,birdsPerDay.length)  ; i++){
+            sum+=birdsPerDay[i];
+        }
+        return sum;
+    }
+
+    public int getBusyDays() {
+        int sum = 0 ;
+        for(int i : birdsPerDay){
+            if(i>=5){
+                sum++;
+            }
+        }
+        return sum;
+    }
+}
