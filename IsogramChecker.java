8b75fd82f7c60c6b45effe546444e48cfed1dfc5
KritagyaChopra <85625671+KritagyaChopra@users.noreply.github.com>
2022-08-16 18:20:45 +0530

    Create IsogramChecker.java

---
+++ IsogramChecker.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class IsogramChecker {
+
+    boolean isIsogram(String phrase) {
+        phrase = phrase.toLowerCase();
+        phrase = phrase.replaceAll("-"," ");
+        char[] ch = phrase.toCharArray();
+        for(int i = 0 ; i < ch.length ; i++){
+            String c = ""+ ch[i];
+            ch[i] = ' ';
+            if(!c.equals(" ")  && new String(ch).contains(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
