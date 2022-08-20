f64cee0fe61cb5ebb1c5c06eb148f7e5a0eaf12e
KritagyaChopra <85625671+KritagyaChopra@users.noreply.github.com>
2022-08-20 22:35:53 +0530

    Update DifferenceOfSquaresCalculator.java

--- DifferenceOfSquaresCalculator.java
+++ DifferenceOfSquaresCalculator.java
@@ -2,12 +2,10 @@ class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
         return ((input*(input+1))/2 ) *((input*(input+1))/2) ;
-        
     }
 
     int computeSumOfSquaresTo(int input) {
         return (input*(input+1) * ((2*input) +1)) / 6 ;
-        
     }
 
     int computeDifferenceOfSquares(int input) {
