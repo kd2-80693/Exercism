93d88c048827c8d37e4c79af0d52d60eb76f9d01
KritagyaChopra <85625671+KritagyaChopra@users.noreply.github.com>
2022-09-07 15:27:45 +0530

    Create Gigasecond.java

---
+++ Gigasecond.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Gigasecond {
+    LocalDateTime date ;
+    public Gigasecond(LocalDate moment) {
+        this.date = moment.atStartOfDay();
+    }
+
+    public Gigasecond(LocalDateTime moment) {
+        this.date = moment ;
+    }
+
+    public LocalDateTime getDateTime() {
+        return this.date.plusSeconds(1000000000L);
+    }
+}
